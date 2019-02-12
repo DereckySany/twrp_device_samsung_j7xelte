370d30947f84c7d956434d4d38a3fa9caff78b75
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-02-12 21:40:19 +0200

    arm64

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-#Bootloader
+# Bootloader
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := universal7870
@@ -11,11 +11,18 @@ TARGET_BOARD_PLATFORM_GPU := mali-t830mp
 
 # Architecture
 
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_CPU_CORTEX_A53 := true
 
 # Kernel

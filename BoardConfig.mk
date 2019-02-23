9f80de24ac7eda150987931a2851e450eaccafe4
yEsPaP <your_email@your_domain.com>
2019-02-23 15:47:07 +0000

    32bit only fekkkkkkkkkkkkkk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,21 +13,14 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Architecture
 
-# 64bit
-TARGET_ARCH := arm64
+# 32bit
+TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
-# 32bit
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-
-# Needed for both
+# CPU stuff
 TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_SMP := true
 

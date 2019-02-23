5c3034119065bc168fd6555b33100317a94126ad
yEsPaP <your_email@your_domain.com>
2019-02-23 12:10:48 +0000

    boardconfig update and use prebuilt path for dt and kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,52 +1,51 @@
 # Bootloader
-
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := universal7870
 
 # Platform
-
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOARD_SOC := exynos7870
 TARGET_BOARD_PLATFORM_GPU := mali-t830mp
 
 # Architecture
 
+# 64bit
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+
+# 32bit
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Needed for both
+TARGET_CPU_CORTEX_A53 := true
 TARGET_CPU_SMP := true
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_CPU_CORTEX_A53 := true
-
 # Kernel
-
+# Kernel is 64bit
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_USES_UNCOMPRESSED_KERNEL := true
 
-TARGET_PREBUILT_KERNEL := device/samsung/j7xelte/kernel
-TARGET_PREBUILT_DTB := device/samsung/j7xelte/dt.img
+# Prebuilts
+TARGET_PREBUILT_KERNEL := device/samsung/j7xelte/prebuilt/Image
+TARGET_PREBUILT_DTB := device/samsung/j7xelte/prebuilt/dt.img
 
 # Boot image
-
 BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/j7xelte/bootimg.mk
-BOARD_KERNEL_IMAGE_NAME := kernel
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_CMDLINE := # Exynos doesn't take cmdline arguments from boot image
+BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-# 000RU = recovery kernel, 000KU = system kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPPE16A000KU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPOL10A000RU
 
 # Partitions
-
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 39845888
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3072000000
@@ -55,14 +54,12 @@ BOARD_CACHEIMAGE_PARTITION_SIZE    := 209715200
 BOARD_FLASH_BLOCK_SIZE := 4096
 
 # File System
-
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 
 # TWRP specific build flags
-RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
@@ -72,8 +69,9 @@ TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
-TW_EXCLUDE_SUPERSU := true
 TW_INCLUDE_NTFS_3G := true
+LZMA_RAMDISK_TARGETS := recovery
+TW_USE_NEW_MINADBD := true
 TW_EXTRA_LANGUAGES := true
 TW_MTP_DEVICE := "/dev/mtp_usb"
 

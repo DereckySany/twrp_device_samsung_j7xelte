a89140208dbbe56def8d45924c51f09f529ad169
yEsPaP <46491239+yEsPaP@users.noreply.github.com>
2019-02-24 09:59:22 +0200

    64/32 mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,12 +13,21 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Architecture
 
-# 32bit
-TARGET_ARCH := arm
+
+
+# 64bit
+TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+
+# 32bit
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # CPU stuff
 TARGET_CPU_CORTEX_A53 := true
